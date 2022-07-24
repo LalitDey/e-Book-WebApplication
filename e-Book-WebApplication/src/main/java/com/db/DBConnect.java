5d6632cfc502584f60f79b231df2080681517eff
Lalitnarayan_Dey <69575504+Lalitnarayan98@users.noreply.github.com>
2022-07-24 18:29:48 +0530

    Update DBConnect.java

--- e-Book-WebApplication/src/main/java/com/db/DBConnect.java
+++ e-Book-WebApplication/src/main/java/com/db/DBConnect.java
@@ -14,7 +14,7 @@ public class DBConnect {
 		try 
 		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebook-webapp", "root", "Lalit99@");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebook-webapp", "root", "pass");
 		} 
 		catch (Exception e) 
 		{
